9472723ab13bd280a075a7319985a8b68ba66d41
thormaxm <116080524+thormaxm@users.noreply.github.com>
2022-10-19 16:02:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/thormaxm/Sm18filter4kdu-1.git /EvaMaria
+  git clone https://github.com/thormaxm/DRAGON.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
