fd33901c5dd312538a32db9bd3fa2d2bbbfb525f
thormaxm <116080524+thormaxm@users.noreply.github.com>
2022-10-19 16:00:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -28,7 +28,7 @@ class script(object):
 ╚══════════════════❍⊱❁"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and THOR will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and DRAGON will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
 1. THOR should have admin privillage.
