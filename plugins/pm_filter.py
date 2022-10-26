bd3fc35c30b62c5c70293e41c5315bc209938456
thormaxm <116080524+thormaxm@users.noreply.github.com>
2022-10-26 12:34:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -751,7 +751,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>Hai 👋 {message.from_urantion}</b> 😍\n\n<b>📁 Found ✨  Files For Your Query : {search} 👇</b>\n ᎮᎧᏇᏋᏒᏋᎴ ᏰᎩ - <b>@TAMIL_FLIMS_HD</b> \n\n"
+        cap = f"<b>Hai 👋 {message.from_urantion}</b> 😍\n\n<b>📁 Found ✨  Files For Your Query : {search} 👇</b>"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
